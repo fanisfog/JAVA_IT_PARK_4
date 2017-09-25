715f194891dc9c313f8fcd0a49e5a2180593a83a
fanisfog <fanisfog@gmail.com>
2017-09-25 22:53:58 +0300

    modification of a file

--- Homeworks/Task2/src/com/company/Main.java
+++ Homeworks/Task2/src/com/company/Main.java
@@ -5,26 +5,86 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int[] numbers = new int[100];
-        int count = 0;
-        int n = 0;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("1.Показать массив - выводит только count-элементов");
-        System.out.println("2.Добавить в начало");
-        System.out.println("3.Добавить в конец");
-        System.out.println("4.Удалить по позиции");
-        System.out.println("5.Выход");
+        //В этой программе ввод пользователя не проверяется!=)
 
-        while (true)
-            switch (scanner.nextInt()) {
+        int array[] = new int[100];
+        int count = 0, choice, input;
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\nМЕНЮ:" +
+                    "1.Показать массив\n" +
+                    "2.Добавить в начало\n" +
+                    "3.Добавить в конец\n" +
+                    "4.Удалить по позиции\n" +
+                    "5.Выход");
+
+            choice = scanner.nextInt();
+            switch (choice) {
                 case 1:
-                    System.out.println("Введите массив из чисел");
-                    n = scanner.nextInt();
-                    numbers[count] = 1;
+                    if (count == 0) {
+                        System.out.println("Пустое хранилище!");
+                        break;
+                    }
+                    System.out.println();
+                    for (int i = 0; i < count; i++)
+                        System.out.print(array[i] + " ");
+                    System.out.println();
+                    break;
+                case 2:
+                    if (count >= 100) {
+                        System.out.println("Хранилище заполнено!");
+                        break;
+                    }
+                    System.out.print("Введите число для добавления в начало: ");
+                    input = scanner.nextInt();
+                    for (int i = count; i > 0; i--) {
+                        array[i] = array[i - 1];
+                    }
+                    array[0] = input;
                     count++;
                     break;
+                case 3:
+                    if (count >= 100) {
+                        System.out.println("Хранилище заполнено!");
+                        break;
+                    }
+                    System.out.print("Введите число для добавления в конец: ");
+                    input = scanner.nextInt();
+                    array[count] = input;
+                    count++;
+                    break;
+                case 4:
+                    if (count <= 0) {
+                        System.out.println("Хранилище уже пустое!");
+                        break;
+                    }
+                    System.out.print("Удалить из позиции: ");
+                    input = scanner.nextInt();
+                    if (input<0||input>99){
+                        System.out.println("Неправильная позиция!!! Удаление не выполнено!");
+                        break;
+                    }else if (input>count-1){
+                        System.out.println("Эта позиция пустая!");
+                        break;
+                    }
+                    for (int i = input; i <= count; i++) {
+                        array[i] = array[i + 1];
+                    }
+                    count--;
+                    System.out.println("Позиция " + input + " удалена");
+                    break;
+                case 5:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Неправильный ввод!");
+                    break;
             }
+        }
+
+
     }
-  
+
 }
 
