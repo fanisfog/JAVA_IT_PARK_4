97675c574feec622f7b4acc9e4e6863302daadeb
fanisfog <fanisfog@gmail.com>
2017-09-25 20:29:38 +0300

    add Tas2

---
+++ Homeworks/Task2/src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[100];
+        int count = 0;
+        int n = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1.Показать массив - выводит только count-элементов");
+        System.out.println("2.Добавить в начало");
+        System.out.println("3.Добавить в конец");
+        System.out.println("4.Удалить по позиции");
+        System.out.println("5.Выход");
+
+        while (true)
+            switch (scanner.nextInt()) {
+                case 1:
+                    System.out.println("Введите массив из чисел");
+                    n = scanner.nextInt();
+                    numbers[count] = 1;
+                    count++;
+                    break;
+            }
+    }
+
+}
+
